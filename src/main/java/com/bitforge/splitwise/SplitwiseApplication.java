25d43c6a2f15a104223fba2b6ce9e58f77aabc5b
Talha Shaikh <17671511+iTzTalha@users.noreply.github.com>
2024-03-06 15:57:47 +0530

    Initial commit

---
+++ src/main/java/com/bitforge/splitwise/SplitwiseApplication.java
@@ -0,0 +1,13 @@
+package com.bitforge.splitwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitforge/splitwise/SplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitforge.splitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

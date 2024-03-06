54ee7674477ca38cdd82b0df7a9c0e725ebbb03c
Talha Shaikh <17671511+iTzTalha@users.noreply.github.com>
2024-03-06 20:56:45 +0530

    modified SplitwiseApplication

--- src/main/java/com/bitforge/splitwise/SplitwiseApplication.java
+++ src/main/java/com/bitforge/splitwise/SplitwiseApplication.java
@@ -1,13 +1,28 @@
 package com.bitforge.splitwise;
 
+import com.bitforge.splitwise.commands.CommandExecutor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class SplitwiseApplication {
+public class SplitwiseApplication implements CommandLineRunner {
+	Scanner sc = new Scanner(System.in);
+	@Autowired
+	CommandExecutor commandExecutor;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SplitwiseApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		while (true) {
+			String input = sc.nextLine();
+			commandExecutor.execute(input);
+		}
+	}
 }
